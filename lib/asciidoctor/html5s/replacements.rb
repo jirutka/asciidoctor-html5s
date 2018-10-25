62f213a776ead055cd7316f3966601fc09cef07b
Jakub Jirutka <jakub@jirutka.cz>
2018-10-25 14:23:04 +0200

    Remove broken support for \--- replacement
    It doesn't work correctly and I have no idea why. However, I don't see
    any sense in this syntax anyway, IMHO it should work exactly opposite
    (avoid substitution when the char is escaped), so nevermind...

--- lib/asciidoctor/html5s/replacements.rb
+++ lib/asciidoctor/html5s/replacements.rb
@@ -4,12 +4,15 @@ module Asciidoctor
   # XXX: Modifies constant defined in Asciidoctor.
   REPLACEMENTS.unshift(
     # foo --- bar -> &thinsp;&mdash;&thinsp;
-    [/(^|\n| |\\)---( |\n|$)/, '&#8201;&#8212;&#8201;', :none],
+    [/(^|\n| )---( |\n|$)/, '&#8201;&#8212;&#8201;', :none],
     # foo---bar -> &mdash;{ZERO WIDTH SPACE}
-    [/(#{CG_WORD})\\?---(?=#{CG_WORD})/, '&#8212;&#8203;', :leading],
+    [/(#{CG_WORD})---(?=#{CG_WORD})/, '&#8212;&#8203;', :leading],
+
     # foo -- bar -> &thinksp;&ndash;&thinsp;
+    # Note: The regexp is copied from Asciidoctor.
     [/(^|\n| |\\)--( |\n|$)/, '&#8201;&#8211;&#8201;', :none],
     # foo--bar -> &ndash;
+    # Note: The regexp is copied from Asciidoctor.
     [/(#{CG_WORD})\\?--(?=#{CG_WORD})/, '&#8211;', :leading],
   )
 end
