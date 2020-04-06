8d56f3f255a0485e92d8d5dc9af6e34e470018d2
Jakub Jirutka <jakub@jirutka.cz>
2020-04-06 15:49:39 +0200

    Don't use our html-pipeline highlighter for other backends

--- lib/asciidoctor/html5s/html_pipeline_highlighter.rb
+++ lib/asciidoctor/html5s/html_pipeline_highlighter.rb
@@ -10,8 +10,22 @@ module Asciidoctor::Html5s
   class HtmlPipelineHighlighter < ::Asciidoctor::SyntaxHighlighter::Base
     register_for 'html-pipeline'
 
-    def format(node, lang, opts)
+    def initialize(name, backend, opts = {})
+      super
+      # Use this version of the #format method only for our backend, otherwise
+      # use the original variant.
+      singleton_class.send(:alias_method, :format, :format_orig) if backend != 'html5s'
+    end
+
+    def format(node, lang, _opts)
       %(<pre><code#{%{ data-lang="#{lang}"} if lang}>#{node.content}</code></pre>)
+    end
+
+    # Copied from Asciidoctor::SyntaxHighlighter::HtmlPipelineAdapter#format.
+    # Note: HtmlPipelineAdapter is not available in asciidoctor.js, that's why
+    # it's done like this instead of delegation or inheritance.
+    def format_orig(node, lang, _opts)
+      %(<pre#{%{ lang="#{lang}"} if lang}><code>#{node.content}</code></pre>)
     end
   end
 end
